68bee699ed13f81325afff532058628f85c88c94
Mateusz <mateusz.obloza.0@gmail.com>
2024-03-28 17:46:30 +0100

    Initial Commit

---
+++ ObjectDetection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ObjectDetection
+//
+//  Created by Mateusz Obłoza on 28/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObjectDetection/ObjectDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectDetectionApp.swift
+//  ObjectDetection
+//
+//  Created by Mateusz Obłoza on 28/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ObjectDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
